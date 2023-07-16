2658ecfac1f4c05531f9260c7a327e91ed5827ba
Ali Alhammouri <alialhammouri5@hotmail.com>
2023-07-16 09:20:12 +0300

    Added jquery and animation

---
+++ script.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+
+    $('#back-to-top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 'fast');
+        return false;
+    });
+});
